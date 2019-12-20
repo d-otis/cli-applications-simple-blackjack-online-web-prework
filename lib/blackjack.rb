c44190559943c50731507410391467b50669a342
Dan Foley <dan@dan-foley.com>
2019-12-20 04:42:40 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -60,12 +60,12 @@ end
 def runner
   welcome
   total = initial_round
-  binding.pry
+  # binding.pry
   until total > 21 do
     total += hit?(total)
-    binding.pry
+    # binding.pry
   end
-  binding.pry
+  # binding.pry
   end_game(total)
   
 end
